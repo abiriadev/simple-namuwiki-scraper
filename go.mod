2e857ebefca4fe77ec19fb69d2525ac1230df271
Abiria <abiria.dev@gmail.com>
2024-01-11 11:43:30 +0900

    feat: initial implementation

---
+++ go.mod
@@ -0,0 +1,40 @@
+module a
+
+go 1.21.5
+
+require (
+	github.com/charmbracelet/log v0.3.1
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/oklog/ulid/v2 v2.1.0
+	github.com/samber/slog-multi v1.0.2
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/samber/lo v1.38.1 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/url"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/log"
+	"github.com/gocolly/colly/v2"
+	"github.com/oklog/ulid/v2"
+	slogmulti "github.com/samber/slog-multi"
+)
+
+const pD = 0660
+const pF = 0644
+
+var wRegex = regexp.MustCompile("^/w/(.*)")
+
+func main() {
+
+	logfName := fmt.Sprintf(
+		"logs/%s_%s.log.ndjson",
+		ulid.Make().String(),
+		time.Now().Format("1-2_3PM:4"),
+	)
+	os.MkdirAll(filepath.Dir(logfName), pD)
+
+	logf, err := os.OpenFile(logfName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, pF)
+	if err != nil {
+		panic(err)
+	}
+
+	slog.SetDefault(
+		slog.New(slogmulti.Fanout(
+			log.NewWithOptions(os.Stdout, log.Options{
+				Level:           log.DebugLevel,
+				ReportTimestamp: true,
+				TimeFormat:      time.Kitchen,
+			}),
+
+			slog.NewJSONHandler(logf, &slog.HandlerOptions{
+				Level: slog.LevelDebug,
+			}),
+		)),
+	)
+
+	domain := "namu.wiki"
+	targetUrl := "https://" + domain
+
+	c := colly.NewCollector(
+		colly.AllowedDomains(domain),
+		// colly.URLFilters(w),
+		colly.MaxDepth(0),
+		colly.CacheDir("./cache"),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+
+		if wRegex.MatchString(link) {
+			text := e.Text
+			rawWord := wRegex.FindStringSubmatch(link)[1]
+			word, _ := url.PathUnescape(rawWord)
+
+			g := e.Request.Ctx.GetAny("graph").(*[]struct {
+				w     string
+				alias string
+			})
+			*g = append(*g, struct {
+				w     string
+				alias string
+			}{word, text})
+
+			slog.Debug("link", "word", word, "alias", text)
+
+			ctx := colly.NewContext()
+			ctx.Put("w", word)
+			newg := make([]struct {
+				w     string
+				alias string
+			}, 0)
+			ctx.Put("graph", &newg)
+
+			c.Request("GET", targetUrl+link, nil, ctx, nil)
+		}
+	})
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Delay:       time.Second,
+		Parallelism: 1,
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		slog.Info("visiting", "w", r.Ctx.Get("w"))
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		w := r.Ctx.Get("w")
+		path := "w/" + w + ".html"
+		os.MkdirAll(filepath.Dir(path), pD)
+		err := r.Save(path)
+		if err != nil {
+			panic(err)
+		}
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		g := r.Ctx.GetAny("graph").(*[]struct {
+			w     string
+			alias string
+		})
+
+		var buf strings.Builder
+		for _, edge := range *g {
+			buf.WriteString(edge.w)
+			buf.WriteString("-->")
+			buf.WriteString(edge.alias)
+			buf.WriteRune('\n')
+		}
+
+		w := r.Ctx.Get("w")
+		path := "w/" + w + ".link"
+		os.WriteFile(path, []byte(buf.String()), 0644)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		slog.Error(err.Error())
+	})
+
+	ctx := colly.NewContext()
+	ctx.Put("w", "나무위키:대문")
+	g := make([]struct {
+		w     string
+		alias string
+	}, 0)
+	ctx.Put("graph", &g)
+	err = c.Request("GET", targetUrl, nil, ctx, nil)
+	if err != nil {
+		panic(err)
+	}
+}
